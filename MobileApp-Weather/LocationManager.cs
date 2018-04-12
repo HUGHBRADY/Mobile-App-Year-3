ba5c67591e276b64f6cc6a0f8ebbc63e9acc6f26
HUGHBRADY <G00338260@gmit.ie>
2018-04-12 12:52:57 +0100

    getPosition coded up

--- MobileApp-Weather/LocationManager.cs
+++ MobileApp-Weather/LocationManager.cs
@@ -9,9 +9,17 @@ namespace MobileApp_Weather
 {
     public class LocationManager
     {
-        public async static void GetLocation()
+        public async static Task<Geoposition> GetPosition()
         {
             var accessStatus = await Geolocator.RequestAccessAsync();
+
+            if (accessStatus != GeolocationAccessStatus.Allowed) throw new Exception();
+                    
+            var geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
+
+            var position = await geolocator.GetGeopositionAsync();
+
+            return position;
         }
     }
 }
