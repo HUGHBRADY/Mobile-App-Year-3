a5bffe9ef21405969ff00b2a709fbd4fcacf8324
HUGHBRADY <G00338260@gmit.ie>
2018-04-12 12:31:50 +0100

    Started geolocation

---
+++ MobileApp-Weather/LocationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace MobileApp_Weather
+{
+    public class LocationManager
+    {
+        public async static void GetLocation()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+        }
+    }
+}
